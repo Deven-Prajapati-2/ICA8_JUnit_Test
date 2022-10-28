9a7874f956095eace2f410662b01e89f475b763a
Deven-Prajapati-2 <dprajap9@asu.edu>
2022-10-27 17:10:44 -0700

    Added fileReader method and it's JUnit testCases

--- src/main/java/Urinals.java
+++ src/main/java/Urinals.java
@@ -1,5 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Urinals {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Urinals urinals = new Urinals();
+        urinals.readFile();
+    }
+    public ArrayList<String> readFile() { // read file and parse all strings
+        String path = "C:/Users/Devsec/Desktop/DevGit/ICA8_JUnit_Test/data/urinal.dat";
+        ArrayList<String> urinalsStrings = new ArrayList<>();
+        try {
+            BufferedReader reader;
+            reader = new BufferedReader(new FileReader(path));
+            String line = reader.readLine();
+            while(line != null) {
+                if(line.equals("-1") || line.equals("EOF")) break;
+                urinalsStrings.add(line);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return urinalsStrings;
+    }
     public Boolean goodString(String str) {  // checks to see if valid string
         int n = str.length();
         for(int i=0;i<n;i++) if(str.charAt(i) != '0' && str.charAt(i) != '1') return false;
--- src/test/java/UrinalsTest.java
+++ src/test/java/UrinalsTest.java
@@ -1,9 +1,23 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UrinalsTest {
-
+    @Test
+    void readFile() {
+        Urinals urinals = new Urinals();
+        System.out.println("====== Deven Prajapati == TEST ONE EXECUTED =======");
+        ArrayList<String> expectedUrinalsStrings = new ArrayList<>();
+        expectedUrinalsStrings.add("10001");
+        expectedUrinalsStrings.add("1001");
+        expectedUrinalsStrings.add("00000");
+        expectedUrinalsStrings.add("0000");
+        expectedUrinalsStrings.add("01000");
+        expectedUrinalsStrings.add("011");
+        assertEquals(expectedUrinalsStrings,urinals.readFile());
+    }
     @Test
     void goodString() {
         Urinals urinals = new Urinals();
